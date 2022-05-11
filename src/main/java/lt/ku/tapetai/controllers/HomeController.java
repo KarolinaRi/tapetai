909b22a50e10900ea8703be55f263eed96c7c2aa
KarolinaRi <k.ripinskyte@gmail.com>
2022-05-11 11:32:09 +0300

    rulonai

--- src/main/java/lt/ku/tapetai/controllers/HomeController.java
+++ src/main/java/lt/ku/tapetai/controllers/HomeController.java
@@ -14,43 +14,26 @@ public class HomeController {
 		return "res";
 	}
 	
-//	@PostMapping("/")
-//	public String kiekiai(@RequestParam("l_kiekis") Integer l_kiekis, @RequestParam("s_kiekis") Integer s_kiekis, 
-//			@RequestParam("d_kiekis") Integer d_kiekis, Model model) {
-//		model.addAttribute("s_kiekis", s_kiekis);
-//		model.addAttribute("d_kiekis", d_kiekis);
-//		model.addAttribute("l_kiekis", l_kiekis);
-//		return "res";
-//	}
-//	
-//	@GetMapping("/duom")
-//	public String duomenys() {
-//		return "res";
-//	}
-	
 	@PostMapping("/duom")
 	public String skaiciavimai(@RequestParam("d_ilgis") double d_ilgis, @RequestParam("s_ilgis") double s_ilgis,
 			@RequestParam("l_aukstis") double l_aukstis, @RequestParam("s_aukstis") double s_aukstis, 
 			@RequestParam("d_aukstis") double d_aukstis, @RequestParam("l_ilgis") double l_ilgis, 
 			@RequestParam("l_kiekis") Integer l_kiekis, @RequestParam("s_kiekis") Integer s_kiekis, 
-			@RequestParam("d_kiekis") Integer d_kiekis, Model model) {
-		model.addAttribute("s_kiekis", s_kiekis);
-		model.addAttribute("d_kiekis", d_kiekis);
-		model.addAttribute("l_kiekis", l_kiekis);
-		model.addAttribute("s_aukstis", s_aukstis);
-		model.addAttribute("d_aukstis", d_aukstis);
-		model.addAttribute("l_aukstis", l_aukstis);
-		model.addAttribute("s_ilgis", s_ilgis);
-		model.addAttribute("d_ilgis", d_ilgis);
-		model.addAttribute("l_ilgis", l_ilgis);
-		
+			@RequestParam("d_kiekis") Integer d_kiekis, Model model,
+			@RequestParam("t_ilgis") double t_ilgis, @RequestParam("t_aukstis") double t_aukstis) {
+	
 		double s_plotas = (s_ilgis*s_aukstis)*s_kiekis;
 		double d_plotas = (d_ilgis*d_aukstis)*d_kiekis;
 		double l_plotas = (l_ilgis*l_aukstis)*l_kiekis;
 		
 		double kiekis = s_plotas-d_plotas-l_plotas;
 		
+		double t_kiekis = kiekis / (t_ilgis*t_aukstis);
+		
 		model.addAttribute("kiekis", kiekis);
+		model.addAttribute("d_plotas", d_plotas);
+		model.addAttribute("l_plotas", l_plotas);
+		model.addAttribute("t_kiekis", t_kiekis);
 		
 		return "result";
 	}
