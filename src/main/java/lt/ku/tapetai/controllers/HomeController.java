550d9b92dd3ed7f3712de8398c65bb25712dd4b6
KarolinaRi <k.ripinskyte@gmail.com>
2022-05-23 09:02:05 +0300

    baigta

--- src/main/java/lt/ku/tapetai/controllers/HomeController.java
+++ src/main/java/lt/ku/tapetai/controllers/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	@GetMapping("/")
 	public String home() {
 		return "home";
@@ -23,22 +23,32 @@ public class HomeController {
 
 	@GetMapping("/duom")
 	public String skaiciavimai() {
-		return "res";
+		return "res2";
 	}
 	
 	@PostMapping("/duom")
-	public String skaiciavimai(@RequestParam("d_ilgis") double d_ilgis, @RequestParam("s_ilgis") double s_ilgis,
-			@RequestParam("l_aukstis") double l_aukstis, @RequestParam("s_aukstis") double s_aukstis, 
-			@RequestParam("d_aukstis") double d_aukstis, @RequestParam("l_ilgis") double l_ilgis, 
-			@RequestParam("l_kiekis") Integer l_kiekis, @RequestParam("s_kiekis") Integer s_kiekis, 
-			@RequestParam("d_kiekis") Integer d_kiekis, Model model,
+	public String skaiciavimai(@RequestParam("s_ilgis1") double s_ilgis1, @RequestParam("s_ilgis2") double s_ilgis2, 
+			@RequestParam("s_ilgis3") double s_ilgis3, @RequestParam("s_ilgis4") double s_ilgis4,
+			@RequestParam("d_ilgis1") double d_ilgis1, @RequestParam("d_ilgis2") double d_ilgis2, 
+			 @RequestParam("l_ilgis1") double l_ilgis1,  @RequestParam("l_ilgis2") double l_ilgis2,
+			@RequestParam("l_ilgis3") double l_ilgis3, @RequestParam("l_aukstis1") double l_aukstis1, 
+			@RequestParam("l_aukstis2") double l_aukstis2, @RequestParam("l_aukstis3") double l_aukstis3,
+			@RequestParam("s_aukstis1") double s_aukstis1, @RequestParam("s_aukstis2") double s_aukstis2,
+			@RequestParam("s_aukstis3") double s_aukstis3, @RequestParam("s_aukstis4") double s_aukstis4,
+			@RequestParam("d_aukstis1") double d_aukstis1, @RequestParam("d_aukstis2") double d_aukstis2, 
+			Model model,
 			@RequestParam("t_ilgis") double t_ilgis, @RequestParam("t_aukstis") double t_aukstis) {
 	
-		double s_plotas = (s_ilgis*s_aukstis)*s_kiekis;
-		double d_plotas = (d_ilgis*d_aukstis)*d_kiekis;
-		double l_plotas = (l_ilgis*l_aukstis)*l_kiekis;
+		double s_plotas = (s_ilgis1*s_aukstis1)+(s_ilgis2*s_aukstis2)+(s_ilgis3*s_aukstis3)+(s_ilgis4*s_aukstis4);
+		s_plotas = Math.round(s_plotas*100)/100.00;
+		double d_plotas = (d_ilgis1*d_aukstis1)+(d_ilgis2*d_aukstis2);
+		d_plotas = Math.round(d_plotas*100)/100.00;
+		double l_plotas = (l_ilgis1*l_aukstis1)+(l_ilgis2*l_aukstis2)+(l_ilgis3*l_aukstis3);
+		l_plotas = Math.round(l_plotas*100)/100.00;
 		double kiekis = s_plotas-d_plotas-l_plotas;
+		kiekis = Math.round(kiekis*100)/100.00;
 		double t_kiekis = kiekis / (t_ilgis*t_aukstis);
+		t_kiekis=Math.round(t_kiekis*100)/100.0;
 		
 		model.addAttribute("kiekis", kiekis);
 		model.addAttribute("d_plotas", d_plotas);
